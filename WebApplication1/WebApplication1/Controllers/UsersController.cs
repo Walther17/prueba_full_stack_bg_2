336d14b6842172ecc17b773458d339ca74d16884
Walther17 <wjom.0942615691@gmail.com>
2026-03-03 20:33:25 -0500

    feat: prueba 2

---
+++ WebApplication1/WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using WebApplication1.Models;
+using System.Text.Json;
+namespace WebApplication1.Controllers
+{
+
+    [Route("Users")]
+    public class UsersController : Controller
+    {
+
+        private readonly HttpClient _httpClient;
+        public readonly string _usersApiUrl;
+        private static List<UserDto> _users = new List<UserDto>();
+        private static List<int> _deletedIds = new();
+        private static int _nextId = 1;
+
+        public UsersController(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _usersApiUrl = configuration["MyApiSettings:UsersUrl"];
+        }
+
+        private void TrimStrings(UserDto model)
+        {
+            model.Email = model.Email?.Trim();
+            model.Username = model.Username?.Trim();
+            model.Password = model.Password?.Trim();
+            model.Phone = model.Phone?.Trim();
+
+            model.Name.Firstname = model.Name.Firstname?.Trim();
+            model.Name.Lastname = model.Name.Lastname?.Trim();
+
+            model.Address.City = model.Address.City?.Trim();
+            model.Address.Street = model.Address.Street?.Trim();
+            model.Address.Zipcode = model.Address.Zipcode?.Trim();
+        }
+
+        // El IActionResult es una interfaz puede retornar (una vista, un JSON, un error 404, un bad request, etc.).
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("~/Views/Users/html/Index.cshtml");
+            return View();
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] UserDto model)
+        {
+            TrimStrings(model);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            model.Id = _nextId++;
+            _users.Add(model);
+
+            return Ok(model);
+        }
+
+        [HttpPut("Edit/{id}")]
+        public async Task<IActionResult> Edit(int id, [FromBody] UserDto model)
+        {
+            TrimStrings(model);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // 1️⃣ Traer todos los usuarios de la API
+            HttpResponseMessage response = await _httpClient.GetAsync(_usersApiUrl);
+            List<UserDto> apiUsers = await response.Content.ReadFromJsonAsync<List<UserDto>>();
+            if (apiUsers == null) apiUsers = new List<UserDto>();
+
+            // 2️⃣ Unir con los locales (_users)
+            List<UserDto> allUsers = apiUsers
+                .Where(u => !_deletedIds.Contains(u.Id)) // ignorar eliminados
+                .Concat(_users)
+                .ToList();
+
+            // 3️⃣ Buscar usuario
+            UserDto user = allUsers.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                return NotFound();
+
+            // 4️⃣ Actualizar campos
+            user.Email = model.Email;
+            user.Username = model.Username;
+            user.Password = model.Password;
+            user.Phone = model.Phone;
+
+            user.Name.Firstname = model.Name.Firstname;
+            user.Name.Lastname = model.Name.Lastname;
+
+            user.Address.City = model.Address.City;
+            user.Address.Street = model.Address.Street;
+            user.Address.Number = model.Address.Number;
+            user.Address.Zipcode = model.Address.Zipcode;
+
+            // 5️⃣ Si era de la API, agregar a locales para poder actualizar
+            if (!_users.Any(u => u.Id == user.Id))
+                _users.Add(user);
+
+            return Ok(user);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Si existe en locales, lo quitamos
+            _users.RemoveAll(u => u.Id == id);
+
+            // Marcamos como eliminado (para los de API)
+            if (!_deletedIds.Contains(id))
+                _deletedIds.Add(id);
+
+            return Ok(new { message = "Usuario eliminado" });
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<ActionResult<PagedResult<UserDto>>> GetUsers(
+            int page = 1,
+            int pageSize = 10,
+            string search = null)
+        {
+            // 🔹 1. Obtener usuarios de API pública
+            HttpResponseMessage response = await _httpClient.GetAsync(_usersApiUrl);
+
+            if (!response.IsSuccessStatusCode)
+                return StatusCode((int)response.StatusCode);
+
+            List<UserDto> apiUsers =
+                await response.Content.ReadFromJsonAsync<List<UserDto>>();
+
+            if (apiUsers == null)
+                apiUsers = new List<UserDto>();
+
+            // 🔹 2. Unir con los locales (_users)
+            List<UserDto> allUsers = apiUsers
+                .Concat(_users)
+                .ToList();
+
+            allUsers = allUsers
+            .Where(u => !_deletedIds.Contains(u.Id))
+            .ToList();
+
+            // 🔹 3. Filtro
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                allUsers = allUsers.Where(u =>
+                    u.Username.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    u.Email.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    u.Name.Firstname.Contains(search, StringComparison.OrdinalIgnoreCase) ||
+                    u.Address.City.Contains(search, StringComparison.OrdinalIgnoreCase)
+                ).ToList();
+            }
+
+            int total = allUsers.Count;
+
+            // 🔹 4. Paginación
+            List<UserDto> pagedUsers = allUsers
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var result = new PagedResult<UserDto>
+            {
+                Data = pagedUsers,
+                TotalRecords = total,
+                Page = page,
+                PageSize = pageSize
+            };
+
+            return Ok(result);
+        }
+
+
+        // ActionResult es una clase base que implementa IActionResult, se usa mucho en Web APIs. Te permite devolver tanto un tipo específico (como una clase Userso) como un resultado HTTP (como NotFound).
+        // Task se usa cuando el método es asíncrono (async).
+        //[HttpGet]
+        //public async Task<ActionResult<Users>> Get()
+        //{
+        //    Users Users = await _service.Get();
+
+        //    if (Users == null)
+        //        return NotFound();
+
+        //    return Users; // No necesitas Ok()
+        //}
+    }
+}
---
+++ WebApplication1/WebApplication1/wwwroot/js/users/index.js
@@ -0,0 +1,397 @@
+﻿
+let currentPage = 1;
+const pageSize = 5;
+let debounceTimer = null;
+
+function loadUsers() {
+
+    const search = document.getElementById("searchInput").value;
+
+    $("#spinner").removeClass("d-none");
+
+    $.ajax({
+        url: '/Users/GetUsers',
+        type: 'GET',
+        data: {
+            page: currentPage,
+            pageSize: pageSize,
+            search: search
+        },
+        success: function (response) {
+
+            renderTable(response.data);
+            renderPagination(response.totalRecords);
+
+            $("#spinner").addClass("d-none");
+        },
+        error: function (error) {
+            $("#spinner").addClass("d-none");
+            showAlert("Error cargando usuarios", "danger");
+            console.error(error);
+        }
+    });
+}
+
+function renderTable(users) {
+
+    let rows = "";
+
+    if (users.length === 0) {
+        rows = `
+            <tr>
+                <td colspan="12" class="text-muted">
+                    No se encontraron registros
+                </td>
+            </tr>
+        `;
+    }
+
+    users.forEach(user => {
+
+        rows += `
+            <tr id="row-${user.id}">
+                <td>${user.id}</td>
+                <td>${user.email}</td>
+                <td>${user.username}</td>
+                <td>********</td>
+                <td>${user.name.firstname}</td>
+                <td>${user.name.lastname}</td>
+                <td>${user.address.city}</td>
+                <td>${user.address.street}</td>
+                <td>${user.address.number}</td>
+                <td>${user.address.zipcode}</td>
+                <td>${user.phone}</td>
+                <td>
+                    <button class="btn btn-sm btn-warning me-1"
+                        onclick='editUser(${JSON.stringify(user)})'>
+                        Editar
+                    </button>
+                    <button class="btn btn-sm btn-danger"
+                        onclick="deleteUser(${user.id})">
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+
+    document.querySelector("#tablaUsuarios tbody").innerHTML = rows;
+}
+
+function renderPagination(totalRecords) {
+
+    let totalPages = Math.ceil(totalRecords / pageSize);
+    let paginationHtml = "";
+
+    if (totalPages <= 1) {
+        $("#paginacion").html("");
+        return;
+    }
+
+    for (let i = 1; i <= totalPages; i++) {
+
+        paginationHtml += `
+                <li class="page-item ${i === currentPage ? "active" : ""}">
+                    <a class="page-link" href="#" onclick="goToPage(${i})">${i}</a>
+                </li>
+            `;
+    }
+
+    $("#paginacion").html(paginationHtml);
+}
+
+window.goToPage = function (page) {
+    currentPage = page;
+    loadUsers();
+}
+document.addEventListener("DOMContentLoaded", function () {
+
+    loadUsers();
+
+    // 🔎 Búsqueda automática con debounce (2 segundos)
+    document.getElementById("searchInput").addEventListener("input", function () {
+
+        clearTimeout(debounceTimer);
+
+        debounceTimer = setTimeout(function () {
+            currentPage = 1;
+            loadUsers();
+        }, 1000);
+
+    });
+
+    const togglePassword = document.getElementById("togglePassword");
+    const passwordInput = document.getElementById("password");
+
+    togglePassword.addEventListener("click", () => {
+        const type = passwordInput.type === "password" ? "text" : "password";
+        passwordInput.type = type;
+        togglePassword.textContent = type === "password" ? "👁️" : "🙈";
+    });
+
+});
+
+let editingUserId = null;
+let deletingUserId = null;
+
+function openCreateModal() {
+
+    editingUserId = null;
+
+    document.getElementById("modalTitle").innerText = "Nuevo Usuario";
+    document.getElementById("userForm").reset();
+
+    $("#idContainer").addClass("d-none");
+
+    new bootstrap.Modal(document.getElementById('userModal')).show();
+}
+
+function editUser(user) {
+
+    editingUserId = user.id;
+
+    document.getElementById("modalTitle").innerText = "Editar Usuario";
+    $("#idContainer").removeClass("d-none");
+
+    userId.value = user.id;
+    email.value = user.email;
+    username.value = user.username;
+    password.value = user.password;
+    firstname.value = user.name.firstname;
+    lastname.value = user.name.lastname;
+    city.value = user.address.city;
+    street.value = user.address.street;
+    number.value = user.address.number;
+    zipcode.value = user.address.zipcode;
+    phone.value = user.phone;
+
+    new bootstrap.Modal(document.getElementById('userModal')).show();
+}
+function saveUser() {
+
+    if (!validateForm()) return;
+
+    const user = {
+        email: email.value.trim(),
+        username: username.value.trim(),
+        password: password.value.trim(),
+        name: {
+            firstname: firstname.value.trim(),
+            lastname: lastname.value.trim()
+        },
+        address: {
+            city: city.value.trim(),
+            street: street.value.trim(),
+            number: parseInt(number.value),
+            zipcode: zipcode.value.trim()
+        },
+        phone: phone.value.trim()
+    };
+
+    let url = editingUserId
+        ? `/Users/Edit/${editingUserId}`
+        : `/Users/Create`;
+
+    let method = editingUserId ? "PUT" : "POST";
+
+    $.ajax({
+        url: url,
+        type: method,
+        contentType: "application/json",
+        data: JSON.stringify(user),
+        success: function (response) {
+
+            if (editingUserId) {
+                updateRow(response);
+                showAlert("Usuario actualizado correctamente", "success");
+            } else {
+                addRow(response);
+                showAlert("Usuario creado correctamente", "success");
+            }
+
+            bootstrap.Modal.getInstance(document.getElementById('userModal')).hide();
+        },
+        error: function (xhr) {
+            showAlert("Error: " + xhr.responseText, "danger");
+        }
+    });
+}
+
+function addRow(user) {
+
+    const tbody = document.querySelector("#tablaUsuarios tbody");
+
+    const row = `
+        <tr>
+            <td>${user.id}</td>
+            <td>${user.email}</td>
+            <td>${user.username}</td>
+            <td>********</td>
+            <td>${user.name.firstname}</td>
+            <td>${user.name.lastname}</td>
+            <td>${user.address.city}</td>
+            <td>${user.address.street}</td>
+            <td>${user.address.number}</td>
+            <td>${user.address.zipcode}</td>
+            <td>${user.phone}</td>
+            <td>
+                <button class="btn btn-sm btn-warning me-1"
+                        onclick="editUser(${user.id})">Editar</button>
+                <button class="btn btn-sm btn-danger"
+                        onclick="deleteUser(${user.id})">Eliminar</button>
+            </td>
+        </tr>
+    `;
+
+    tbody.insertAdjacentHTML("afterbegin", row);
+}
+
+function updateRow(user) {
+
+    const row = document.getElementById(`row-${user.id}`);
+
+    row.innerHTML = `
+        <td>${user.id}</td>
+        <td>${user.email}</td>
+        <td>${user.username}</td>
+        <td>********</td>
+        <td>${user.name.firstname}</td>
+        <td>${user.name.lastname}</td>
+        <td>${user.address.city}</td>
+        <td>${user.address.street}</td>
+        <td>${user.address.number}</td>
+        <td>${user.address.zipcode}</td>
+        <td>${user.phone}</td>
+        <td>
+            <button class="btn btn-sm btn-warning me-1"
+                onclick='editUser(${JSON.stringify(user)})'>
+                Editar
+            </button>
+            <button class="btn btn-sm btn-danger"
+                onclick="deleteUser(${user.id})">
+                Eliminar
+            </button>
+        </td>
+    `;
+}
+
+function deleteUser(id) {
+
+    deletingUserId = id;
+
+    new bootstrap.Modal(document.getElementById('deleteModal')).show();
+}
+
+function confirmDelete() {
+
+    $.ajax({
+        url: `/Users/Delete/${deletingUserId}`,
+        type: "DELETE",
+        success: function () {
+            loadUsers();
+            bootstrap.Modal.getInstance(document.getElementById('deleteModal')).hide();
+            showAlert("Usuario eliminado", "success");
+        },
+        error: function () {
+            showAlert("Error eliminando usuario", "danger");
+        }
+    });
+}
+
+function validateForm() {
+
+    const emailVal = email.value.trim();
+    const usernameVal = username.value.trim();
+    const passwordVal = password.value.trim();
+    const firstnameVal = firstname.value.trim();
+    const lastnameVal = lastname.value.trim();
+    const cityVal = city.value.trim();
+    const streetVal = street.value.trim();
+    const numberVal = number.value.trim();
+    const zipcodeVal = zipcode.value.trim();
+    const phoneVal = phone.value.trim();
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const usernameRegex = /^[a-zA-Z0-9._-]{4,30}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d).{8,64}$/;
+    const zipcodeRegex = /^[A-Za-z0-9 -]{3,12}$/;
+    const phoneRegex = /^[0-9()+ -]{7,20}$/;
+    console.log("passwordVal");
+    console.log("passwordVal", passwordVal);
+    if (!emailVal) {
+        showAlert("Email es requerido.", "danger");
+        return false;
+    }
+
+    if (emailVal.length > 100) {
+        showAlert("Email máximo 100 caracteres.", "danger");
+        return false;
+    }
+
+    if (!emailRegex.test(emailVal)) {
+        showAlert("Email debe tener formato válido (ej: usuario@email.com).", "danger");
+        return false;
+    }
+
+    if (!usernameRegex.test(usernameVal)) {
+        showAlert("Username debe tener 4-30 caracteres. Solo letras, números y ._- sin espacios.", "danger");
+        return false;
+    }
+
+    if (!passwordRegex.test(passwordVal)) {
+        showAlert("Password debe tener 8-64 caracteres, al menos 1 letra y 1 número.", "danger");
+        return false;
+    }
+
+    if (firstnameVal.length < 2 || firstnameVal.length > 50) {
+        showAlert("Nombre debe tener entre 2 y 50 caracteres.", "danger");
+        return false;
+    }
+
+    if (lastnameVal.length < 2 || lastnameVal.length > 50) {
+        showAlert("Apellido debe tener entre 2 y 50 caracteres.", "danger");
+        return false;
+    }
+
+    if (cityVal.length < 2 || cityVal.length > 60) {
+        showAlert("Ciudad debe tener entre 2 y 60 caracteres.", "danger");
+        return false;
+    }
+
+    if (streetVal.length < 2 || streetVal.length > 100) {
+        showAlert("Calle debe tener entre 2 y 100 caracteres.", "danger");
+        return false;
+    }
+
+    if (!numberVal || parseInt(numberVal) < 1) {
+        showAlert("Número debe ser un entero mayor o igual a 1.", "danger");
+        return false;
+    }
+
+    if (!zipcodeRegex.test(zipcodeVal)) {
+        showAlert("Zipcode debe tener entre 3 y 12 caracteres alfanuméricos.", "danger");
+        return false;
+    }
+
+    if (!phoneRegex.test(phoneVal)) {
+        showAlert("Phone debe tener entre 7 y 20 caracteres. Solo números y símbolos (+ - ()).", "danger");
+        return false;
+    }
+
+    return true;
+}
+
+function showAlert(message, type) {
+
+    const alertHtml = `
+        <div class="alert alert-${type} alert-dismissible fade show position-fixed top-0 end-0 m-3 shadow"
+             role="alert" style="z-index:9999;">
+            ${message}
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+        </div>
+    `;
+
+    $("body").append(alertHtml);
+
+    setTimeout(() => $(".alert").alert('close'), 3000);
+}
